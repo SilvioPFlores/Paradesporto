9187b57938c3c21d65719ebb2f6eb845b6d25000
silvio flores <silvio.flores@unifesp.brgit config --global user.name silvio>
2025-09-16 11:28:52 -0300

    Correção para só carregar a sessão se ela já existir

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
 include 'config/config.php';
 include 'includes/head.php';
 include 'includes/menu.php';
-$nivel = $_SESSION['repositorio']['nivel'];
+
+$nivel = isset($_SESSION['repositorio']['nivel']) ?? '';
 if ($nivel != '') {
     getHead('Administrativo');
     getMenu($nivel);
