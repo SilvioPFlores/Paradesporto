9cce82d76c8d516160fe00632487bc91ccf9e950
silvio flores <silvio.flores@unifesp.brgit config --global user.name silvio>
2025-09-18 00:33:15 -0300

    Adicionado o icone com o link para accessercise

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ menu();
     </div-->
     <div class="row row row-cols-1 row-cols-md-5 g-4 top40">
         <div class="col">
-            <a href="https://anchor.fm/paradesporto"  target="_blank">
+            <a href="https://anchor.fm/paradesporto" target="_blank">
                 <div class="card text-center cardHome responsive-bord h-100">
                     <div class="card-body responsive-bory-card">
                         <img class="card-img-top" src="img/home/podcast.png" alt="">
@@ -33,7 +33,7 @@ menu();
                 <div class="card text-center cardHome responsive-bord rounded-4 h-100">
                     <div class="card-body responsive-bory-card">
                         <img class="card-img-top" src="img/home/paradesporto.png" alt="">
-                        <strong class="responsive-font-example"><?=$lang['reposit']?></strong>
+                        <strong class="responsive-font-example"><?= $lang['reposit'] ?></strong>
                     </div>
                 </div>
             </a>
@@ -43,7 +43,7 @@ menu();
                 <div class="card text-center cardHome responsive-bord rounded-4 h-100">
                     <div class="card-body responsive-bory-card">
                         <img class="card-img-top" src="img/home/logo-youtube.png" alt="">
-                        <strong class="responsive-font-example"><?=$lang['canal']?></strong>
+                        <strong class="responsive-font-example"><?= $lang['canal'] ?></strong>
                     </div>
                 </div>
             </a>
@@ -53,7 +53,7 @@ menu();
                 <div class="card text-center cardHome responsive-bord rounded-4 h-100">
                     <div class="card-body responsive-bory-card">
                         <img class="card-img-top" src="img/home/producao.png" alt="">
-                        <strong class="responsive-font-example"><?=$lang['producao']?></strong>
+                        <strong class="responsive-font-example"><?= $lang['producao'] ?></strong>
                     </div>
                 </div>
             </a>
@@ -63,17 +63,30 @@ menu();
                 <div class="card text-center cardHome responsive-bord rounded-4 h-100">
                     <div class="card-body responsive-bory-card">
                         <img class="card-img-top" src="img/home/paradesporto.png" alt="">
-                        <strong class="responsive-font-example"><?=$lang['projeto']?></strong>
+                        <strong class="responsive-font-example"><?= $lang['projeto'] ?></strong>
                     </div>
                 </div>
             </a>
         </div>
     </div>
-    <div class="divInsta top40">
-        <a href="https://www.instagram.com/paradesporto_br/" target="_blank">
-            <img src="img/home/logo-insta.png" alt="" >
-            <strong>@paradesporto_br</strong>
-        </a>
+    <div class="container text-center my-4">
+        <div class="row justify-content-center align-items-center g-3">
+            <div class="col-12 col-sm-auto">
+                <div class="divInsta">
+                    <a href="https://www.instagram.com/paradesporto_br/" target="_blank" class="d-inline-flex align-items-center gap-2 text-decoration-none">
+                        <img src="img/home/logo-insta.png" alt="Instagram">
+                        <strong>@paradesporto_br</strong>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-auto">
+                <div class="divAccess">
+                    <a href="https://www.instagram.com/paradesporto_br/" target="_blank">
+                        <img src="img/home/access.png" alt="Accessercise">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?php
