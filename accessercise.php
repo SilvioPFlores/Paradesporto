42f3a500765c9d842dfe9c40cc33dcf64133bc93
silvio flores <silvio.flores@unifesp.brgit config --global user.name silvio>
2025-09-18 23:50:35 -0300

    Modificação das imagens para ficar com fundo transparente e posicionamento da imagem celular na mao

--- accessercise.php
+++ accessercise.php
@@ -8,42 +8,34 @@ include 'includes/menu.php';
 menu('ACCESSERCISE');
 
 ?>
-<div class="container">
-    <div class="text-center">
-        <img src="img/home/accessQr.png" style="width:200px;">
-        <div class="divAccess">
-            <a href="https://accessercise.com/">https://accessercise.com/</a>
+<div class="container my-5">
+    <div class="row align-items-center">
+        <!-- Coluna com a imagem do celular -->
+        <div class="col-12 col-md-4 text-center mb-4 mb-md-0">
+            <img src="img/home/accessercise-mao.png" class="img-fluid" alt="Accessercise App">
         </div>
-        <br>
-        <h3>
-            <?=$lang['bemVindo']?>
-        </h3>
-        <p class="h5">
-            <?=$lang['parag_1']?>
-        </p>
-        <p class="h5">
-            <?=$lang['parag_2']?>
-        </p>
-        <p class="h5">
-            <?=$lang['parag_3']?>
-        </p>
-        <br>
-    </div>
-    <div class="container text-center my-4">
-        <div class="row justify-content-center align-items-center g-5">
-            <div class="col-12 col-sm-auto">
-                <div class="divDispGoogleApple">
-                    <a href="#" target="_blank">
-                        <img src="img/home/dispGoogle.png" alt="Instagram">
-                    </a>
-                </div>
+
+        <!-- Coluna com QR, link e textos -->
+        <div class="col-12 col-md-8 text-center">
+            <img src="img/home/accessQr.png" style="width:150px;" alt="QR Code">
+            <div class="divAccess my-3">
+                <a href="https://accessercise.com/" target="_blank" class="h4 d-block">
+                    https://accessercise.com/
+                </a>
             </div>
-            <div class="col-12 col-sm-auto">
-                <div class="divDispGoogleApple">
-                    <a href="#">
-                        <img src="img/home/dispApple.png" alt="Accessercise">
-                    </a>
-                </div>
+            <h3><?=$lang['bemVindo']?></h3>
+            <p class="h5"><?=$lang['parag_1']?></p>
+            <p class="h5"><?=$lang['parag_2']?></p>
+            <p class="h5"><?=$lang['parag_3']?></p>
+
+            <!-- Botões App Store e Google Play -->
+            <div class="d-flex justify-content-center gap-3 mt-4 flex-wrap">
+                <a href="#" target="_blank">
+                    <img src="img/home/dispGoogle.png" alt="Google Play" class="img-fluid" style="max-height:60px;">
+                </a>
+                <a href="#" target="_blank">
+                    <img src="img/home/dispApple.png" alt="App Store" class="img-fluid" style="max-height:60px;">
+                </a>
             </div>
         </div>
     </div>
